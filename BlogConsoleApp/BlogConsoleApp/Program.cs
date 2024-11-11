475711321f12208ad71fb3ec9b362b4442b71715
BextiyarNovruzov <baxtiyarbn-bp215@code.edu.az>
2024-11-11 12:38:23 +0400

    BlogsDb database ile connecting qurub C# da Console App yazdim.

---
+++ BlogConsoleApp/BlogConsoleApp/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Core.Data;
+using Microsoft.Data.SqlClient;
+using static Azure.Core.HttpHeader;
+namespace BlogConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            string connetionString = "Server=PROUDD\\SQLEXPRESS;Database=BlogsDB;Trusted_Connection=True;TrustServerCertificate=True;";
+            using(SqlConnection connection = new SqlConnection(connetionString))
+            {
+                connection.Open();
+                Console.WriteLine("BlogsDb Databese ile connection ugurlu oldu))");
+
+                bool TrueValue = true;
+                bool TrueValue1 = true;
+                while(TrueValue)
+                {
+                    Console.WriteLine("\n======================= Blog App Menu ========================");
+                    Console.WriteLine("1: USERS");
+                    Console.WriteLine("2: Blogs And Users");
+                    Console.WriteLine("3: Blogs and Catagoryes");
+                    Console.WriteLine("4: User Comments");
+                    Console.WriteLine("5: Blog Users");
+                    Console.WriteLine("6: Blogs Count");
+                    Console.WriteLine("7: Blogs Table");
+                    Console.WriteLine("0: Exiting");
+                    Console.Write("Bir secim edin: ");
+                    string choice = Console.ReadLine();
+                    switch (choice)
+                    {
+                        case "1":
+                            Console.Clear();
+                            Console.WriteLine("-------------------------- USERS  ------------------------------------");
+                            AppDbContext.GetUsersData(connection);
+                            break;
+
+                        case "2":
+                            Console.Clear();
+                            Console.WriteLine("-------------------------- Blogs And Users ------------------------------------");
+                            AppDbContext.GetBlogsUsersData(connection);
+                            break;
+                        
+                        case "3":
+                            Console.Clear();
+                            Console.WriteLine("-------------------------- Blogs and Catagoryes ------------------------------------");
+                            AppDbContext.GetBlogsCatagoryesData(connection);
+                            break;
+
+                        case "4":
+                            Console.Clear();
+                            Console.WriteLine("-------------------------- User Comments ------------------------------------");
+                            Console.WriteLine("Commentini gormek istediyiniz userin ID - sini daxil edin.");
+                            TrueValue = int.TryParse(Console.ReadLine(), out int UserId);
+                            {
+                                if (!TrueValue) { Console.WriteLine("Int tipinde Id daxil edin."); }
+                            }
+                            AppDbContext.GetUserCommentData(connection, UserId);
+                            break;
+
+                        case "5":
+                            Console.Clear();
+                            Console.WriteLine("-------------------------- Blog Users ------------------------------------");
+                            Console.WriteLine("Bloglarini gormek istediyiniz userin ID - sini daxil edin.");
+                            TrueValue1 = int.TryParse(Console.ReadLine(), out  int UserID);
+                            {
+                                if (!TrueValue1) { Console.WriteLine("Int tipinde Id daxil edin."); }
+                            }
+                            AppDbContext.GetBlogersData(connection, UserID);
+                            break;
+
+                        case "6":
+                            Console.Clear();
+                            Console.WriteLine("-------------------------- Blogs Count ------------------------------------");
+                            Console.WriteLine("Blog sayini gormek  istediyiniz Categorynin ID - sini daxil edin.");
+                            TrueValue1 = int.TryParse(Console.ReadLine(), out int CategoryID);
+                            {
+                                if (!TrueValue1) { Console.WriteLine("Int tipinde Id daxil edin."); }
+                            }
+                            Console.WriteLine($"{CategoryID} ID-li categoriyada {AppDbContext.GetBlogCountByCategoryId(connection, CategoryID)} sayda blog var.");
+                            break;
+
+                        case "7":
+                            Console.Clear();
+                            Console.WriteLine("-------------------------- Blogs Table ------------------------------------");
+                            Console.WriteLine("Blog sayini gormek  istediyiniz Categorynin ID - sini daxil edin.");
+                            TrueValue1 = int.TryParse(Console.ReadLine(), out int userId);
+                            {
+                                if (!TrueValue1) { Console.WriteLine("Int tipinde Id daxil edin."); }
+                            }
+                            AppDbContext.GetBlogsTable(connection, userId);
+                            break;
+
+                        case "0":
+                            Console.Clear();
+                            TrueValue = false;
+                            Console.WriteLine("Proqramdan Cixildi)");
+                            break;
+
+                        default:
+                            Console.Clear();
+                            Console.WriteLine("Duzgun secim edin zahmet deyilse...");
+                            break;
+                    }
+
+                }
+              
+            }
+
+        }
+    }
+}
---
+++ BlogConsoleApp/Core/Data/AppDbContext.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Data.SqlClient;
+
+namespace Core.Data
+{
+    public class AppDbContext
+    {
+
+        //Movcud USerlerin butun datalarini gostermekk
+        public static void GetUsersData(SqlConnection connection)
+        {
+            string Usersquery = "Select* from Users";
+            using (SqlCommand command1 = new SqlCommand(Usersquery, connection))
+            using (SqlDataReader reader1 = command1.ExecuteReader())
+            {
+                while (reader1.Read())
+                {
+                    Console.WriteLine($"User ID: {reader1["ID"]}, User Name : {reader1["UserName"]}, FullName: {reader1["FullName"]}, Age: {reader1["Age"]} ");
+                }
+            }
+        }
+        //1. Blogs'un Title, Users'in userName ve fullName columnlarini qaytaran view yazirsiniz.
+        public static void GetBlogsUsersData(SqlConnection connection)
+        {
+            string query = "select * from InfoViewBloger";
+            using (SqlCommand command = new SqlCommand(query, connection))
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine($"Blog Titles: {reader["Blog Titles"]},  Bloger User Name: {reader["Bloger User Name"]},  Bloger Full Name: {reader["Bloger User Name"]}  ");
+                }
+            }
+        }
+        //2. Blogs'un Title, Categories'in Name columnlarini qaytaran view.
+        public static void GetBlogsCatagoryesData(SqlConnection connection)
+        {
+            string query = "select * from BlogsAndCatagoryNames";
+            using (SqlCommand command = new SqlCommand(query, connection))
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine($"Blog Titles: {reader["Blog Titles"]}, Catagory Names{reader["Catagory Names"]}");
+                }
+            }
+        }
+
+        //3. @userId parametri qebul edib hemin parametre uygun userin yazdigi commentleri qaytaran procedure yazirsiniz.
+
+        public static void GetUserCommentData(SqlConnection connection, int UserId)
+        {
+            using (SqlCommand command = new SqlCommand("usp_get_comment_content", connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@userId", UserId);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($" User Id : {reader["User Id"]}, User Full Name : {reader["User Full Name"]},  Comment : {reader["Comment content"]}");
+                    }
+                }
+
+
+            }
+        }
+
+        //4. @userId parametri qebul edib hemin parametre uygun userin bloglarini qaytaran procedure yazirsiniz.
+
+        public static void GetBlogersData(SqlConnection connection, int UserID)
+        {
+            using (SqlCommand command = new SqlCommand("usp_get_user_blogs", connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@userid", UserID);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($" User Id : {reader["User Id"]}, User Full Name : {reader["User Full Name"]},  Blogs title : {reader["Blogs title"]}");
+
+                    }
+                }
+
+            }
+        }
+        //5. Parametr olaraq, @categoryId qebul edib, hemin parametre Bloglarin sayini geri qaytaran function yazirsiniz.
+
+        public static int GetBlogCountByCategoryId(SqlConnection connection, int CategoryID)
+        {
+            int BlogsCount = 0;
+            string query = "select dbo.getCountBlogs(@categoryId) as 'Blogs Count'";
+            ;
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@categoryId", CategoryID);
+                BlogsCount = (int)command.ExecuteScalar();
+
+            }
+
+            return BlogsCount;
+
+        }
+        //6. Parametr olaraq, @userId qebul edib, hemin user'in yaratdigi bloglari table kimi geri qaytaran function yazirsiniz.
+
+        public static void GetBlogsTable(SqlConnection connection, int userId)
+        {
+            string query = "select * from GetBlogTable(@userid)";
+            ;
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@userid", userId);
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"BlogId: {reader["BlogId"]}, BlogTitle: {reader["BlogTitle"]}, BlogDescription: {reader["BlogDescription"]}");
+                    }
+                }
+            }
+
+
+        }
+    }
+}
